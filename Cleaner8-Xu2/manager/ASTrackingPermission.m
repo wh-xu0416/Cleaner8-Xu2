abe43270531998392621f72a7d5133d032eeb7be
小徐 <9288933+xu-wenhao11@user.noreply.gitee.com>
2026-01-13 15:29:22 +0800

    aat

---
+++ Cleaner8-Xu2/manager/ASTrackingPermission.m
@@ -0,0 +1,67 @@
+#import "ASTrackingPermission.h"
+#import <AppTrackingTransparency/AppTrackingTransparency.h>
+
+static NSString * const kASHasRequestedATTKey = @"hasRequestedATT";
+
+@implementation ASTrackingPermission
+
++ (BOOL)hasRequested {
+    return [[NSUserDefaults standardUserDefaults] boolForKey:kASHasRequestedATTKey];
+}
+
++ (BOOL)shouldRequest {
+    if (@available(iOS 14, *)) {
+        if ([self hasRequested]) return NO;
+        return (ATTrackingManager.trackingAuthorizationStatus == ATTrackingManagerAuthorizationStatusNotDetermined);
+    }
+    return NO;
+}
+
++ (NSInteger)currentStatusValue {
+    if (@available(iOS 14, *)) {
+        return (NSInteger)ATTrackingManager.trackingAuthorizationStatus;
+    }
+    return 0;
+}
+
++ (void)requestIfNeededWithDelay:(NSTimeInterval)delay
+                      completion:(ASTrackingAuthCompletion)completion {
+
+    if (![self shouldRequest]) {
+        if (completion) completion([self currentStatusValue]);
+        return;
+    }
+
+    static BOOL sRequesting = NO;
+    @synchronized(self) {
+        if (sRequesting) {
+            if (completion) completion([self currentStatusValue]);
+            return;
+        }
+        sRequesting = YES;
+    }
+
+    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kASHasRequestedATTKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+
+    if (@available(iOS 14, *)) {
+        void (^doRequest)(void) = ^{
+            [ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(ATTrackingManagerAuthorizationStatus status) {
+                @synchronized(self) { sRequesting = NO; }
+                if (completion) completion((NSInteger)status);
+            }];
+        };
+
+        if (delay > 0) {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delay * NSEC_PER_SEC)),
+                           dispatch_get_main_queue(), doRequest);
+        } else {
+            dispatch_async(dispatch_get_main_queue(), doRequest);
+        }
+    } else {
+        @synchronized(self) { sRequesting = NO; }
+        if (completion) completion([self currentStatusValue]);
+    }
+}
+
+@end
